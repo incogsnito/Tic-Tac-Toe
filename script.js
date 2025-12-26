1b57e5637d91035afcc87af85ae55583e918837d
Samad <Samad.o@outlook.com>
2025-12-26 17:49:04 -0500

    overall code structure complete.
    Will resume work on project sometime today or tomorrow

---
+++ script.js
@@ -0,0 +1,15 @@
+const turn = document.querySelector('#turn');
+
+const x = 'X'
+const o = 'O'
+
+
+document.querySelector('.container').addEventListener('click', function(e){
+  const target = e.target;
+  const targetContent = document.createElement('p');
+  target.appendChild(targetContent);
+
+
+  
+}, );
+
